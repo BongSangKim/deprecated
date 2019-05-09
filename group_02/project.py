cbd2fa1f85c603f112d01d5200cdf6f578f61e54
BongSangKim <tph0231@gmail.com>
2019-05-09 16:59:29 +0900

    t SNE 코드 추가

--- group_02/project.py
+++ group_02/project.py
@@ -46,7 +46,6 @@ print(dt.wv['war'])
 dt.wv.most_similar(positive='people')
 
 
-
 import gensim 
 from os import walk
 
@@ -102,7 +101,55 @@ for _ in range(100):
     del wordlist[idx]
 ####
 #최빈출 워드 출력 코드
+#=============================test- t-SNE=====================================    
+#=============================test- t-SNE=====================================
+#=============================test- t-SNE=====================================
+keys = ['america', 'korea', 'muslims', 'world']
+
+embedding_clusters = []
+word_clusters = []
+for word in keys:
+    embeddings = []
+    words = []
+    for similar_word, _ in tp_model.wv.most_similar(word, topn=50): ###model부분 바꾸기
+        words.append(similar_word)
+        embeddings.append(tp_model.wv[similar_word])                ##model 부분 바꾸기
+    embedding_clusters.append(embeddings)
+    word_clusters.append(words)
     
-    
+from sklearn.manifold import TSNE
+import numpy as np
+
+embedding_clusters = np.array(embedding_clusters)
+n, m, k = embedding_clusters.shape
+tsne_model_en_2d = TSNE(perplexity=15, n_components=2, init='pca', n_iter=3500, random_state=32)
+embeddings_en_2d = np.array(tsne_model_en_2d.fit_transform(embedding_clusters.reshape(n * m, k))).reshape(n, m, 2)
+
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+
+def tsne_plot_similar_words(title, labels, embedding_clusters, word_clusters, a, filename=None):
+    plt.figure(figsize=(16, 9))
+    colors = cm.rainbow(np.linspace(0, 1, len(labels)))
+    for label, embeddings, words, color in zip(labels, embedding_clusters, word_clusters, colors):
+        x = embeddings[:, 0]
+        y = embeddings[:, 1]
+        plt.scatter(x, y, alpha = a, label = label)
+        for i, word in enumerate(words):
+            plt.annotate(word, alpha=0.5, xy=(x[i], y[i]), xytext=(5, 2),
+                         textcoords='offset points', ha='right', va='bottom', size=8)
+    plt.legend(loc=4)
+    plt.title(title)
+    plt.grid(True)
+    if filename:
+        plt.savefig(filename, format='png', dpi=300, bbox_inches='tight')
+    plt.show()
+
+
+tsne_plot_similar_words('Similar words', keys, embeddings_en_2d, word_clusters, 0.7,
+                        'similar_words.png')
+#=============================test- t-SNE=====================================
+#=============================test- t-SNE=====================================
 ###추가할 코드###
 '''그람 슈미츠 코드'''    
